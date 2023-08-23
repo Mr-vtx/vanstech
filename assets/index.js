36f0e66f0f7d3f08a36ecf2478070a5a945e34e8
Wisdom <127138565+chibuikewis@users.noreply.github.com>
2023-08-23 14:41:23 +0100

    Update index.js

--- assets/index.js
+++ assets/index.js
@@ -1,6 +1,6 @@
 //sidebar variables
 
-const sidebar = document.querySelector("[data-sidebar]");
+const sidebar = document.querySelector("[data-sidebar]"); 
 const sidebarBtn = document.querySelector("[data-sidebar-btn]");
 
 // sidebar toggle functionality for mobile
@@ -13,13 +13,39 @@ function gotowhatsapp() {
   var subject = document.getElementById("subject").value;
   var email = document.getElementById("email").value;
   var message = document.getElementById("message").value;
-
-  var url = "https://wa.me/9121058131?text="
+  let pattern = /^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/;
+  
+  if (name==null || name=="") {
+    name.focus();
+    return false;
+  } 
+  if (subject==null ||subject=="") {
+    subject.focus();
+    return false;
+  }
+  if (email==null || email=="") {
+    email.focus();
+    pattern = function (){
+      function isEmail(email) { 
+        return true;
+      }
+      return true;
+  }
+    
+    return false;
+  }
+  if (message==null ||message=="") {
+    message.focus();
+    return false;
+  }
+  else {
+    var url = "https://wa.me/9121058131?text="
       + "Name: " + name + "%0a"
       + "Subject: " + subject + "%0a"
       + "Email: " + email + "%0a"
       + "Messsage: " + message;
 
   window.open(url, '_blank').focus();
-}
+  }
 
+}
